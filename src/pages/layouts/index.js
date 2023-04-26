8173235b5bc0298ff0b9273e95aff4ad97eb7e6a
5aad <saad_anwer@live.com>
2023-04-26 14:06:57 +0500

    fix issue layout and screen 3

--- src/pages/layouts/index.js
+++ src/pages/layouts/index.js
@@ -1,23 +1,26 @@
 import Link from "next/link";
 import React from "react";
 
-const Layouts = () => {
+const Layouts = (props) => {
   return (
-    <div className="navbar">
-      <Link href="/">
-        <button className="nav-button">Home</button>
-      </Link>
-      <Link href="/profile">
-        <button className="nav-button">Profile</button>
-      </Link>
+    <div>
+      <div className="navbar">
+        <Link href="/">
+          <button className="nav-button">Home</button>
+        </Link>
+        <Link href="/profile">
+          <button className="nav-button">Profile</button>
+        </Link>
 
-      <button className="nav-button">Experience</button>
-      <Link href="/portfolio">
-        <button className="nav-button">Portfolio</button>
-      </Link>
-      <Link href="/contact">
-        <button className="nav-button">Contact</button>
-      </Link>
+        <button className="nav-button">Experience</button>
+        <Link href="/portfolio">
+          <button className="nav-button">Portfolio</button>
+        </Link>
+        <Link href="/contact">
+          <button className="nav-button">Contact</button>
+        </Link>
+      </div>
+      {props.children}
     </div>
   );
 };
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -5,67 +5,61 @@ import { Col, Row, Typography } from "antd";
 
 const Profile = () => {
   return (
-    <>
+    <Layouts>
       <div className={styles.container}>
-        <Layouts />
-        <div className={styles.container1}>
-          <Row className={styles.row}>
-            <Col className={styles.col1}>
-              <img
-                src="/assets/images/insta.png"
-                className={styles.img}
-                alt="No Image Found"
-              />
-              <img
-                src="/assets/images/linkd.png"
-                className={styles.img}
-                alt="No Image Found"
-              />
-              <img
-                src="/assets/images/youtube.png"
-                className={styles.img}
-                alt="No Image Found"
-              />
-            </Col>
-            <Col className={styles.col2}>
-              <img
-                height={250}
-                width={250}
-                src="/assets/images/profile.png"
-                className={styles.img}
-                alt="No Image Found"
-              />
-            </Col>
-            <Col className={styles.col3}>
-              <Row>
-                <h1 className={styles.title1}>Saad Hassam</h1>
-              </Row>
-              <Row>
-                <h1 className={styles.title2}>Designer/Concept Artist</h1>
-              </Row>
-              <Row>
-                <p className={styles.para}>
-                  I began my journey in the world of art, creating realistic
-                  paintings and drawings. But as time went on, my artistic
-                  vision began to evolve. I found myself exploring the abstract,
-                  experimenting with simple shapes, and pushing the boundaries
-                  of what was possible. Through my career, I never stopped
-                  creating art, always striving to create something new and
-                  unique.
-                </p>
-                <p className={styles.para} style={{ marginTop: "3rem" }}>
-                  My creative journey eventually led me to the world of concept
-                  art, where I designed characters for local films. I also
-                  discovered a love for theatre, taking a six-month training
-                  session in acting that culminated in performances on some of
-                  the biggest stages around
-                </p>
-              </Row>
-            </Col>
-          </Row>
-        </div>
+        <Row justify="space-between" className={styles.container1}>
+          <Col span={2}>
+            <img
+              src="/assets/images/insta.png"
+              className={styles.img}
+              alt="No Image Found"
+            />
+            <br />
+            <img
+              src="/assets/images/linkd.png"
+              className={styles.img}
+              alt="No Image Found"
+            />
+            <br />
+            <img
+              src="/assets/images/youtube.png"
+              className={styles.img}
+              alt="No Image Found"
+            />
+          </Col>
+          <Col span={3}>
+            <img
+              height={150}
+              width={150}
+              src="/assets/images/profile.png"
+              className={styles.img}
+              alt="No Image Found"
+            />
+          </Col>
+          <Col span={19}>
+            <h1 className={styles.title1}>Saad Hassam</h1>
+
+            <h1 className={styles.title2}>Designer/Concept Artist</h1>
+
+            <p className={styles.para}>
+              I began my journey in the world of art, creating realistic
+              paintings and drawings. But as time went on, my artistic vision
+              began to evolve. I found myself exploring the abstract,
+              experimenting with simple shapes, and pushing the boundaries of
+              what was possible. Through my career, I never stopped creating
+              art, always striving to create something new and unique.
+            </p>
+            <p className={styles.para} style={{ marginTop: "3rem" }}>
+              My creative journey eventually led me to the world of concept art,
+              where I designed characters for local films. I also discovered a
+              love for theatre, taking a six-month training session in acting
+              that culminated in performances on some of the biggest stages
+              around
+            </p>
+          </Col>
+        </Row>
       </div>
-    </>
+    </Layouts>
   );
 };
 
